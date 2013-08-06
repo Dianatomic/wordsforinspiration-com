a42ed966d008e8f7e432e6a5286c6b3bc4625862
Diana Arz <diana@tractical.com>
2013-08-05 21:18:42 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WordsforinspirationCom::Application.config.secret_token = 'bc289744bd7af1a775940cb3c0230a88fa7247a300edcb0fa788a99fb2d54bec8e2879f8518b0a10995d85f96f6166376137e1f5edbdc739e33b2c41a4355aaa'
